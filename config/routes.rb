6404e7cf6ebdc0d278b70b76e7cf9f4e5cc02bea
Kevin Rodriguez <kyrodriguez97@gmail.com>
2017-05-21 04:39:31 +0000

    Users sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   resources :users, only: [:new, :create]
   resources :products
   root 'products#index'
